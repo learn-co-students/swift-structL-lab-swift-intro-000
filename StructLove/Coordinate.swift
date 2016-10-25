315d706a73a37c24d03363214fb18be3fcc25dc0
RGero215 <rgero215@gmail.com>
2016-10-25 05:10:59 -0400

    Done.

--- StructLove/Coordinate.swift
+++ StructLove/Coordinate.swift
@@ -8,6 +8,33 @@
 
 import Foundation
 
+struct Coordinate {
+    var latitude : Double
+    var longitude : Double
+    
+    var isInNorthernHemisphere : Bool {
+        return latitude >= 0.0
+    }
+    var isInSouthernHemisphere : Bool {
+        return !isInNorthernHemisphere
+    }
+    var isInWesternHemisphere : Bool {
+        return longitude >= 0.0
+    }
+    var isInEasternHemisphere : Bool {
+        return !isInWesternHemisphere
+    }
+    
+    
+        
+    func distanceTo(coordinate: Coordinate) -> Double {
+        let R = 6371e3
+        let lat1 = latitude.radians
+        let lat2 = coordinate.latitude.radians
+        let long = (longitude - coordinate.longitude).radians
+        return acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(long)) * R / 1000
+    }
+}
 
 
 
