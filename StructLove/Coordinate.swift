11a3ff41d933fccc8228535c6bf48ba0bfd8ab38
Surendar S <surendars@Surendars-MacBook-Pro.local>
2016-10-31 13:14:33 +0530

    completed

--- StructLove/Coordinate.swift
+++ StructLove/Coordinate.swift
@@ -9,7 +9,58 @@
 import Foundation
 
 
-
+struct Coordinate {
+    
+    var latitude:Double
+    var longitude:Double
+    
+    var isInNorthernHemisphere:Bool{
+    
+        return latitude > 0
+            
+    }
+    
+    var isInSouthernHemisphere:Bool{
+        
+        return latitude < 0
+        
+    }
+    var isInWesternHemisphere:Bool{
+        
+        return longitude > 0
+        
+    }
+    var isInEasternHemisphere:Bool{
+        
+        return longitude < 0
+        
+    }
+    
+    
+    func distance(to coordinate:Coordinate)->Double{
+    
+    //acos(sin(latitude1) * sin(latitude2) + cos(latitude1) * cos(latitude2) * cos(longitude1-longitude2)) * 6371000 / 1000.
+        
+       let latitude1 = self.latitude.radians
+       let longitude1 = self.longitude.radians
+       let latitude2 = coordinate.latitude.radians
+       let longitude2 = coordinate.longitude.radians
+        
+        
+        return acos(sin(latitude1) * sin(latitude2) + cos(latitude1) * cos(latitude2) * cos(longitude1-longitude2)) * 6371000 / 1000
+        
+        
+    }
+    
+    
+    init(latitude:Double,longitude:Double){
+        
+        self.latitude = latitude
+        self.longitude = longitude
+        
+    }
+    
+}
 
 
 
@@ -24,3 +75,13 @@ extension Double {
         return self * M_PI / 180
     }
 }
+
+
+
+
+
+
+
+
+
+
