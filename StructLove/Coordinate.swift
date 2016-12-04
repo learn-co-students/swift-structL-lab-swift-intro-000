c2007efaaa5e96d7dec5a7df1f7dbf44e806062e
halolsi <noriogura900@gmail.com>
2016-12-04 11:52:12 -0800

    Struct Lab

--- StructLove/Coordinate.swift
+++ StructLove/Coordinate.swift
@@ -8,7 +8,59 @@
 
 import Foundation
 
+struct Coordinate {
+    var latitude : Double
+    var longitude : Double
+    var isInNorthernHemisphere : Bool {
+        if latitude > 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    var isInSouthernHemisphere : Bool {
+        if latitude < 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    var isInWesternHemisphere : Bool {
+        if longitude > 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    var isInEasternHemisphere : Bool {
+        if longitude < 0 {
+            return true
+        } else {
+            return false
+        }
+    }
 
+    init(latitude: Double, longitude : Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+    func distance(to: Coordinate) -> Double {
+        let coord2 = to
+        var coord_distance : Double
+        let latitude2 : Double = coord2.latitude.radians
+        let longitude2 : Double = coord2.longitude.radians
+        let latitude1 : Double = self.latitude.radians
+        let longitude1 : Double = self.longitude.radians
+        
+        
+        coord_distance = acos(sin(latitude1) * sin(latitude2) + cos(latitude1) * cos(latitude2) * cos(longitude1-longitude2)) * 6371000.0/1000.0
+        
+        //coord_distance = 12187.117
+        
+        return coord_distance
+    }
+}
 
 
 
--- StructLove/PizzaDeliveryService.swift
+++ StructLove/PizzaDeliveryService.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+struct PizzaDeliveryService {
+    var location : Coordinate
+    var pizzasAvailable : Int
+    
+    init(location : Coordinate) {
+        self.location = location
+        self.pizzasAvailable = 10
+    }
+    
+    func isInRange(to : Coordinate) -> Bool {
+        let destination : Coordinate = to
+        
+        if destination.distance(to: self.location) <= 5000 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    mutating func deliverPizza(to: Coordinate) -> Bool{
+        let destination = to
+        if self.isInRange(to: destination) && self.pizzasAvailable > 0 {
+            self.pizzasAvailable = self.pizzasAvailable - 1
+            return true
+        } else {
+            return false
+        }
+    }
+}
