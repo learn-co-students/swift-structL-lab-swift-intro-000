d56694cf684ddb2f94be66c67b6b08c0f3c42c5f
Subcipher <krishna@javaworkgroup.com>
2016-12-24 20:51:09 -0500

    completed struct pizza

--- StructLove/Coordinate.swift
+++ StructLove/Coordinate.swift
@@ -9,10 +9,6 @@
 import Foundation
 
 
-
-
-
-
 // Any double type has now the radians computed property available to it thanks to this extension.
 
 // For Example 
@@ -24,3 +20,42 @@ extension Double {
         return self * M_PI / 180
     }
 }
+
+struct Coordinate{
+    var latitude:Double
+    var longitude:Double
+    
+    init(latitude:Double,longitude:Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+        var isInNorthernHemisphere:Bool{
+            return latitude > 0
+        }
+        var isInSouthernHemisphere:Bool{
+            return latitude < 0
+        }
+        var isInWesternHemisphere:Bool{
+            return longitude > 0
+        }
+        var isInEasternHemisphere:Bool{
+            return longitude < 0
+        }
+
+    func distance(to coordinate:Coordinate)->Double{
+        let long1 = self.longitude.radians
+        let lat1 = self.latitude.radians
+        let long2 = coordinate.longitude.radians
+        let lat2 = coordinate.latitude.radians
+        let distance = acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(long1 - long2)) * 6371000/1000
+    return distance
+    }
+}
+
+
+
+
+
+
+
+
--- StructLove/PizzaDeliveryService.swift
+++ StructLove/PizzaDeliveryService.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+struct PizzaDeliveryService {
+    var location:Coordinate
+    var  pizzasAvailable:Int
+    
+    init(location:Coordinate){
+        self.location = location
+        self.pizzasAvailable  = 10
+    }
+    
+    func isInRange(to distination:Coordinate)->Bool{
+        let calDistance = location.distance(to: distination) //distination.distance (to: location)
+    
+    return (calDistance < 5000)
+    }
+
+    mutating func deliverPizza(to distination:Coordinate)->Bool{
+        
+        if isInRange(to: distination) && pizzasAvailable > 0 {
+            pizzasAvailable -= 1
+            return true
+        } else {
+            return false
+        }
+    }
+}
