dfdb205b81c929b0b9e94eed536dbfd4b60114d5
KircheisHe <hechenzhang@outlook.com>
2017-01-04 21:42:46 -0800

    Complete Task

--- StructLove/Coordinate.swift
+++ StructLove/Coordinate.swift
@@ -24,3 +24,41 @@ extension Double {
         return self * M_PI / 180
     }
 }
+
+struct Coordinate {
+    var latitude : Double;
+    var longitude : Double;
+    
+    init(latitude : Double, longitude : Double) {
+        self.latitude = latitude;
+        self.longitude = longitude;
+    }
+    
+    var isInNorthernHemisphere : Bool {
+        get {
+            return self.latitude > 0
+        }
+    }
+    
+    var isInSouthernHemisphere : Bool {
+        get {
+            return self.latitude < 0
+        }
+    }
+    
+    var isInWesternHemisphere : Bool {
+        get {
+            return self.longitude > 0
+        }
+    }
+    
+    var isInEasternHemisphere : Bool {
+        get {
+            return self.longitude < 0
+        }
+    }
+    
+    func distance(to : Coordinate) -> Double {
+        return acos(sin(self.latitude.radians) * sin(to.latitude.radians) + cos(self.latitude.radians) * cos(to.latitude.radians) * cos(self.longitude.radians - to.longitude.radians)) * 6371000 / 1000
+    }
+}
