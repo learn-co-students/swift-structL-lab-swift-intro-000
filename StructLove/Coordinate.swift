a78800b19b0c1e15d8c0921d056b07dcba69dedd
Blake Minho Kim <blakeminhokim@Blakes-MacBook-Pro.local>
2017-01-06 00:30:02 -0500

    finished

--- StructLove/Coordinate.swift
+++ StructLove/Coordinate.swift
@@ -9,7 +9,57 @@
 import Foundation
 
 
-
+struct Coordinate {
+    var latitude: Double
+    var longitude: Double
+    
+    var isInNorthernHemisphere: Bool {
+        if latitude > 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    var isInSouthernHemisphere: Bool {
+        if latitude < 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    var isInWesternHemisphere: Bool {
+        if longitude > 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    var isInEasternHemisphere: Bool {
+        if latitude < 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+   
+    
+    init(latitude: Double, longitude: Double){
+        self.latitude = latitude
+        self.longitude = longitude
+        
+    }
+    
+    func distance(to coordinate: Coordinate) -> Double {
+        return acos(sin(self.latitude.radians)*sin(coordinate.latitude.radians)+acos(self.latitude.radians)*acos(coordinate.latitude.radians)*acos(self.longitude.radians-coordinate.longitude.radians))*6371000/1000
+    }
+    
+    
+    
+    
+}
 
 
 
--- StructLove/PizzaDeliveryService.swift
+++ StructLove/PizzaDeliveryService.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+struct PizzaDeliveryService {
+    var location: Coordinate
+    var pizzasAvailable = 10
+    
+    init(location: Coordinate){
+        self.location = location
+    }
+    
+    
+    func isInRange(to destination: Coordinate) -> Bool {
+        if location.distance(to: destination) < 5000 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    
+    mutating func deliverPizza(to destination: Coordinate) -> Bool {
+        if isInRange(to: destination) && pizzasAvailable > 0 {
+            pizzasAvailable = pizzasAvailable - 1
+            return true
+        } else{
+            return false
+        }
+    }
+    
+    
+    
+}
