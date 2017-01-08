d84fa7004a688d3c2feec47ef94281d0fb26d8b0
JJJV <juan@jaramillo.net>
2017-01-07 19:29:07 -0600

    Lab result
    done.

--- StructLove/Coordinate.swift
+++ StructLove/Coordinate.swift
@@ -8,7 +8,57 @@
 
 import Foundation
 
+struct Coordinate {
+    var latitude: Double
+    var longitude: Double
+    
+    var isInNorthernHemisphere: Bool {
+        
+        if latitude > 0 {
+            return true
+        }
+        return false
+    }
+    
+    var isInSouthernHemisphere: Bool {
+        if latitude < 0 {
+            return true
+        }
+        return false
+    }
+    
+    
+    var isInWesternHemisphere : Bool {
+        if longitude > 0 {
+            return true
+        }
+        return false
+    }
+    
+    var isInEasternHemisphere: Bool {
+        if longitude < 0 {
+            return true
+        }
+        return false
+    }
 
+    init (latitude: Double, longitude: Double) {
+        
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+
+    func distance(to coordinate: Coordinate) -> Double {
+        
+        let la1 = self.latitude.radians
+        let lo1 = self.longitude.radians
+        let la2 = coordinate.latitude.radians
+        let lo2 = coordinate.longitude.radians
+        
+        return acos(sin(la1)*sin(la2) + cos(la1)*cos(la2)*cos((lo1 - lo2)))*6371000/1000
+    }
+
+    }
 
 
 
--- StructLove/Person.swift
+++ StructLove/Person.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+struct Person {
+    
+    let firstName: String
+    let lastName: String
+    let home: Coordinate
+    var isHungry: Bool
+    
+    
+    init ( firstName: String, lastName: String, home: Coordinate) {
+        
+        self.firstName = firstName
+        self.lastName = lastName
+        self.home = home
+        isHungry = true
+        
+    }
+    
+    
+    mutating func eatPizza(from service: PizzaDeliveryService) -> Bool {
+        
+        if service.isInRange(to: home) && isHungry {
+          isHungry = false
+            return true
+        }
+        return false
+    }
+    
+    
+}
