8cf4df060e2830be9332495a9cb7fc7b4cf687a4
4Ring <ukrainianslavic@gmail.com>
2017-01-27 13:31:24 -0800

    done

--- StructLove/Coordinate.swift
+++ StructLove/Coordinate.swift
@@ -8,19 +8,39 @@
 
 import Foundation
 
-
-
-
-
-
-// Any double type has now the radians computed property available to it thanks to this extension.
-
-// For Example 
-// let number = 5.0
-// number.radians
-
 extension Double {
     var radians: Double {
         return self * M_PI / 180
     }
 }
+
+struct Coordinate {
+    let latitude: Double
+    let longitude: Double
+    
+    var isNorthernHemisphere: Bool {
+        return latitude >= 0.0
+    }
+    
+    var isSoutherHemisphere: Bool {
+        return latitude <= 0.0
+    }
+    
+    var isWesternHemisphere: Bool {
+        return longitude >= 0.0
+    }
+    
+    var isEasterHemisphere: Bool {
+        return longitude <= 0.0
+    }
+
+    func distanceTo(coordinate: Coordinate) -> Double {
+        let R = 6371e3
+        let φ1 = latitude.radians
+        let φ2 = coordinate.latitude.radians
+        let Δλ = (longitude - coordinate.longitude).radians
+        return acos(sin(φ1) * sin(φ2) + cos(φ1) * cos(φ2) * cos(Δλ)) * R / 1000
+    }
+}
+
+
--- StructLove/PizzaDeliveryService.swift
+++ StructLove/PizzaDeliveryService.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+struct PizzaDeliveryService {
+    var location: Coordinate
+    var pizzasAvailable: Int
+    
+    init(location: Coordinate) {
+        self.location = location
+        self.pizzasAvailable = 10
+    }
+    
+    func isInRange(destination: Coordinate) -> Bool {
+        return location.distanceTo(coordinate: destination) <= 5000.0
+    }
+    
+    mutating func deliverPizzaTo(destination: Coordinate) -> Bool {
+        guard pizzasAvailable > 0 else { return false }
+        guard isInRange(destination: destination) else { return false }
+        pizzasAvailable -= 1
+        return true
+    }
+}
