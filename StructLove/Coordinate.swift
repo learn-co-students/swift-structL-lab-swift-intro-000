ba7d059c74579c49c1898efd1c0f8003db68c3bf
Marc Aupont <marc@digimarktech.com>
2017-02-08 15:42:38 -0500

    Completed StructL Lab with all tests passing

--- StructLove/Coordinate.swift
+++ StructLove/Coordinate.swift
@@ -8,7 +8,49 @@
 
 import Foundation
 
-
+struct Coordinate {
+    
+    let latitude: Double
+    let longitude: Double
+    
+    init(latitude: Double, longitude: Double) {
+        
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+    var isInNorthernHemisphere: Bool {
+        
+        return latitude > 0
+        
+    }
+    
+    var isInSouthernHemisphere: Bool {
+        
+        return latitude < 0
+        
+    }
+    
+    var isInWesternHemisphere: Bool {
+        
+        return longitude > 0
+    }
+    
+    var isInEasternHemisphere: Bool {
+        
+        return longitude < 0
+    }
+    
+    func distance(to coordinate: Coordinate) -> Double {
+        
+        let latitude1 = self.latitude.radians
+        let latitude2 = coordinate.latitude.radians
+        let longitude1 = self.longitude.radians
+        let longitude2 = coordinate.longitude.radians
+        
+        return acos(sin(latitude1) * sin(latitude2) + cos(latitude1) * cos(latitude2) * cos(longitude1 - longitude2)) * 6371000 / 1000
+    }
+}
 
 
 
--- StructLove/PizzaDeliveryService.swift
+++ StructLove/PizzaDeliveryService.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+struct PizzaDeliveryService {
+    
+    let location: Coordinate
+    var pizzasAvailable: Int
+    
+    init(location: Coordinate) {
+        
+        self.location = location
+        self.pizzasAvailable = 10
+    }
+    
+    func isInRange(to destination: Coordinate) -> Bool {
+        
+        return location.distance(to: destination) <= 5000
+    }
+    
+    mutating func deliverPizza(to destination: Coordinate) -> Bool {
+        
+        if pizzasAvailable == 0 {
+            
+            return false
+        }
+        
+        if isInRange(to: destination) {
+            
+            pizzasAvailable -= 1
+            return true
+        }
+        
+        return false
+    }
+    
+}
