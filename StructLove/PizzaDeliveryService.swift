880983f667ff3eee56dd386c9fd266093118c5d3
Ben <bennalle1@gmail.com>
2017-03-06 11:21:02 -0500

    passes

--- StructLove/PizzaDeliveryService.swift
+++ StructLove/PizzaDeliveryService.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+
+struct PizzaDeliveryService {
+    var location: Coordinate
+    var pizzasAvailable: Int = 10
+    
+    init(location: Coordinate) {
+        self.location = location
+    }
+    
+    func isInRange(to destination: Coordinate) -> Bool {
+        if location.distance(to: destination) > 5000 {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    mutating func deliverPizza(to destination: Coordinate) -> Bool {
+        if location.distance(to: destination) <= 5000 && pizzasAvailable > 1 {
+            pizzasAvailable -= 1
+            return true
+        } else if pizzasAvailable == 0 {
+            return false
+        } else {
+            return false
+        }
+    }
+    
+    
+}
