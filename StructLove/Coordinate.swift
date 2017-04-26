05d7eb82aa53b7c67d74de42ec6b0e90bb665240
brianpoole <game.programmer@gmail.com>
2017-04-26 13:25:07 -0700

    solved

--- StructLove/Coordinate.swift
+++ StructLove/Coordinate.swift
@@ -8,6 +8,32 @@
 
 import Foundation
 
+struct Coordinate{
+    var latitude: Double
+    var longitude: Double
+    var isInNorthernHemisphere: Bool{
+        return latitude >= 0
+    }
+    var isInSouthernHemisphere: Bool{
+        return latitude < 0
+    }
+    var isInWesternHemisphere: Bool{
+        return longitude >= 0
+    }
+    var isInEasternHemisphere: Bool{
+        return longitude < 0
+    }
+    
+    init(latitude: Double, longitude: Double){
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+    func distance(to coordinate: Coordinate) -> Double{
+             //acos(sin(latitude1) * sin(latitude2) + cos(latitude1) * cos(latitude2) * cos(longitude1-longitude2)) * 6371000 / 1000
+        return acos(sin(self.latitude.radians) * sin(coordinate.latitude.radians) + cos(self.latitude.radians) * cos(coordinate.latitude.radians) * cos(self.longitude.radians - coordinate.longitude.radians)) * 6371000 / 1000
+    }
+}
 
 
 
--- StructLove/PizzaDeliveryService.swift
+++ StructLove/PizzaDeliveryService.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+struct PizzaDeliveryService{
+    var location: Coordinate
+    var pizzasAvailable: Int = 10
+    
+    init(location: Coordinate){
+        self.location = location
+    }
+    
+    func isInRange(to destination: Coordinate) -> Bool{
+        if(location.distance(to: destination) <= 5000){
+            return true
+        }
+        return false
+    }
+    
+    mutating func deliverPizza(to destination: Coordinate) -> Bool {
+        if(self.pizzasAvailable > 0 && isInRange(to: destination)){
+            self.pizzasAvailable -= 1
+            return true
+        }
+        return false
+    }
+}
