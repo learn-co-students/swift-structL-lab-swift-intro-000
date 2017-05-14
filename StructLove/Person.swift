af1735bb00e4094a20a354e641f6b487e98ac121
one-for-all <jianan.li@yahoo.com>
2017-05-13 20:00:00 -0700

    completed struct lab

--- StructLove/Person.swift
+++ StructLove/Person.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+//Finally, you have to create a Person struct, so someone can eat all these pizzas.
+//In Person.swift, declare a new struct called Person.
+//Add four properties to your Person struct:
+//A String called firstName
+//A String called lastName
+//A Coordinate called home
+//A Bool called isHungry
+//Create an initializer that takes three parameters: a String called firstName, a String called lastName, and a Coordinate called home. These parameters should be assigned to the appropriate properties. isHungry should initially be set to true.
+//Create a method called eatPizza(from:). This method takes one parameter, a PizzaDeliveryService called service that represents where the pizza is being ordered from. If the person is hungry and the service is in range of the person's home, then the isHungry property should be set to false and the method should return true. If the person is not hungry or the service is not in range of the person's home, the method should simply return false.
+
+struct Person {
+    var firstName: String
+    var lastName: String
+    var home: Coordinate
+    var isHungry: Bool = true
+    init(firstName: String, lastName: String, home: Coordinate) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.home = home
+    }
+    mutating func eatPizza(from service: PizzaDeliveryService) -> Bool {
+        if isHungry && service.isInRange(to: home) {
+            isHungry = false
+            return true
+        }
+        return false
+    }
+}
