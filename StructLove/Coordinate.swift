a71102880929645842719125d43f6f9e9239abc9
Joe Kim <josephkim0224@gmail.com>
2017-05-22 15:38:02 -0400

    Finished Lab

--- StructLove/Coordinate.swift
+++ StructLove/Coordinate.swift
@@ -8,7 +8,33 @@
 
 import Foundation
 
-
+struct Coordinate {
+    var latitude: Double
+    var longitude: Double
+    var isInNorthernHemisphere: Bool {
+        return latitude > 0
+    }
+    var isInSouthernHemisphere: Bool {
+        return latitude < 0
+    }
+    
+    var isInWesternHemisphere: Bool {
+        return longitude > 0
+    }
+    
+    var isInEasternHemisphere: Bool {
+        return longitude < 0
+    }
+    
+    init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+    func distance(to coordinate: Coordinate) -> Double {
+        return acos(sin(latitude.radians) * sin(coordinate.latitude.radians) + cos(latitude.radians) * cos(coordinate.latitude.radians) * cos(longitude.radians - coordinate.longitude.radians)) * 6371000 / 1000
+    }
+}
 
 
 
--- StructLove/PizzaDeliveryService.swift
+++ StructLove/PizzaDeliveryService.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+struct PizzaDeliveryService {
+    var location: Coordinate
+    var pizzasAvailable: Int
+    
+    init(location: Coordinate) {
+        self.location = location
+        self.pizzasAvailable = 10
+    }
+    
+    func isInRange(to destination: Coordinate) -> Bool {
+        return location.distance(to: destination) <= 5000
+    }
+    
+    mutating func deliverPizza(to destination: Coordinate) -> Bool {
+        if pizzasAvailable == 0 {
+            return false
+        } else if isInRange(to: destination) {
+            pizzasAvailable -= 1
+            return true
+        } else {
+            return false
+        }
+    }
+}
