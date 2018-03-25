4e9a55ab889167c1471bc6af7f62c9349503a596
ericjlein <35666587+ericjlein@users.noreply.github.com>
2018-03-25 02:07:38 -0400

    Add files via upload

--- StructLove/Person.swift
+++ StructLove/Person.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+struct Person {
+    let firstName: String
+    let lastName: String
+    let home: Coordinate
+    var isHungry: Bool = true
+    
+    init(firstName: String, lastName: String, home: Coordinate) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.home = home
+    }
+    
+    mutating func eatPizza(from service: PizzaDeliveryService) -> Bool {
+        switch self.isHungry && service.isInRange(to: home) {
+        case true:
+            self.isHungry = false
+            return true
+        case false:
+            return false
+        }
+//        if self.isHungry && service.isInRange(destination: home) {
+//            isHungry = false
+//            return true
+//        }
+//        else if !self.isHungry || !service.isInRange(destination: home) {
+//            return false
+//        }
+    }
+}
+
+
--- StructLove/PizzaDeliveryService.swift
+++ StructLove/PizzaDeliveryService.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+struct PizzaDeliveryService {
+    let location: Coordinate
+    var pizzasAvailable: Int
+    
+    init(location: Coordinate) {
+        self.location = location
+        self.pizzasAvailable = 10
+    }
+    
+    func isInRange(to destination: Coordinate) -> Bool {
+        if destination.distance(to: location) <= 5000.00 {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    
+    mutating func deliverPizza(to destination: Coordinate) -> Bool {
+        switch pizzasAvailable {
+        case 0:
+            return false
+        default:
+            switch isInRange(to: destination) {
+            case true:
+                pizzasAvailable -= 1
+                return true
+            case false:
+                return false
+            }
+        }
+    }
+    
+        
+}
