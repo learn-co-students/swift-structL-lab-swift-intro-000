c6bd83f22b80d04bba9235eb545b658a0c598774
mjanoudy <muaawiaj1991@hotmail.com>
2018-10-04 11:59:38 +0300

    Finished Structs Lab

--- StructLove/Person.swift
+++ StructLove/Person.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+struct Person {
+    var firstName: String
+    var lastName: String
+    var home: Coordinate
+    var isHungry: Bool = true
+    
+    init(firstName: String, lastName: String, home: Coordinate) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.home = home
+    }
+    
+    mutating func eatPizza(service: PizzaDeliveryService)-> Bool{
+        if service.isInRange(to: self.home) && self.isHungry{
+            isHungry = false
+            return true
+        } else {
+            return false
+        }
+    }
+}
