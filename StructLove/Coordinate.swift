6fe523009b68e4857f5b6d0b9ca9ac9057d3cefe
bodeacorina <corina.bodea@yahoo.com>
2018-10-16 11:08:26 +0300

    Add structL solution

--- StructLove/Coordinate.swift
+++ StructLove/Coordinate.swift
@@ -8,10 +8,37 @@
 
 import Foundation
 
-
-
-
-
+struct Coordinate {
+    var latitude: Double
+    var longitude: Double
+    
+    init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+    var isInNorthernHemisphere: Bool {
+        return latitude > 0
+    }
+    var isInSouthernHemisphere: Bool {
+        return latitude < 0
+    }
+    var isInWesternHemisphere: Bool {
+        return longitude > 0
+    }
+    var isInEasternHemisphere: Bool {
+        return longitude < 0
+    }
+    
+    func distance(to coordinate: Coordinate) -> Double {
+        let latitude1 = self.latitude.radians
+        let latitude2 = coordinate.latitude.radians
+        let longitude1 = self.longitude.radians
+        let longitude2 = coordinate.longitude.radians
+        
+        return acos(sin(latitude1) * sin(latitude2) + cos(latitude1) * cos(latitude2) * cos(longitude1-longitude2)) * 6371000 / 1000
+    }
+}
 
 // Any double type has now the radians computed property available to it thanks to this extension.
 
@@ -21,6 +48,6 @@ import Foundation
 
 extension Double {
     var radians: Double {
-        return self * M_PI / 180
+        return self * .pi / 180
     }
 }
--- StructLove/PizzaDeliveryService.swift
+++ StructLove/PizzaDeliveryService.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+struct PizzaDeliveryService {
+    var location: Coordinate
+    var pizzaAvailable: Int
+    
+    init(location: Coordinate) {
+        self.location = location
+        pizzaAvailable = 10
+    }
+    
+    func isInRange(to destination: Coordinate) -> Bool {
+        return destination.distance(to: self.location) <= 5000
+    }
+    
+    mutating func deliverPizza(to destination: Coordinate)-> Bool{
+        if self.isInRange(to: destination) && pizzaAvailable > 0 {
+            pizzaAvailable -= 1
+            return true
+        } else {
+            return false
+        }
+    }
+}
